ae815b716abf9eb90a3ee1d04438e4756531704a
Yasin Muhammed Tukur <saukakke@gmail.com>
2026-08-28 09:54:52 +0100

    Add safe demo database seeder

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,4 @@
+<?php
+namespace Database\Seeders;
+use Illuminate\Database\Seeder;use App\Models\{User,Exam};use Illuminate\Support\Facades\Hash;
+class DatabaseSeeder extends Seeder {public function run():void{$admin=User::updateOrCreate(['email'=>'admin@cbt.local'],['name'=>'System Administrator','password'=>Hash::make('ChangeMe123!'),'role'=>'admin']);$teacher=User::updateOrCreate(['email'=>'teacher@cbt.local'],['name'=>'Demo Teacher','password'=>Hash::make('ChangeMe123!'),'role'=>'teacher']);User::updateOrCreate(['email'=>'student@cbt.local'],['name'=>'Demo Student','password'=>Hash::make('ChangeMe123!'),'role'=>'student']);Exam::updateOrCreate(['title'=>'CBT Demo Examination'],['description'=>'Sample examination created by the seed data.','duration_minutes'=>30,'created_by'=>$teacher->id,'is_published'=>true]);}}
