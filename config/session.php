281a28632aa9779e67a605bd4458cf7f42319592
Yasin Muhammed Tukur <saukakke@gmail.com>
2026-08-28 09:56:16 +0100

    Add session configuration

---
+++ config/session.php
@@ -0,0 +1,2 @@
+<?php
+return ['driver'=>env('SESSION_DRIVER','database'),'lifetime'=>120,'expire_on_close'=>false,'encrypt'=>false,'files'=>storage_path('framework/sessions'),'connection'=>env('SESSION_CONNECTION'),'table'=>'sessions','store'=>env('SESSION_STORE'),'lottery'=>[2,100],'cookie'=>env('SESSION_COOKIE','cbt_session'),'path'=>'/','domain'=>env('SESSION_DOMAIN'),'secure'=>env('SESSION_SECURE_COOKIE'),'http_only'=>true,'same_site'=>'lax','partitioned'=>false];
