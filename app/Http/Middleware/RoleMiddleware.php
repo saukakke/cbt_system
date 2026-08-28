45586eeb2e88977b09109a15fa3ed498135d7eb9
Yasin Muhammed Tukur <saukakke@gmail.com>
2026-08-28 09:53:52 +0100

    Add role authorization middleware

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,4 @@
+<?php
+namespace App\Http\Middleware;
+use Closure; use Illuminate\Http\Request; use Symfony\Component\HttpFoundation\Response;
+class RoleMiddleware { public function handle(Request $request,Closure $next,...$roles):Response { if(!$request->user()||!in_array($request->user()->role,$roles,true)) abort(403); return $next($request); } }
